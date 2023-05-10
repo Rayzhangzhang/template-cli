c9892077ef5d5fe428589b679a1df9af75ed1a5f
Rayzhangzhang <1069319374@qq.com>
2023-05-10 13:56:09 +0800

    feat: add react18

---
+++ src/template/framework/react18/vite/vite.config.js
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      cors: true,
+      host: '0.0.0.0',
+      port: 8080,
+      proxy: {},
+    },
+  };
+});
