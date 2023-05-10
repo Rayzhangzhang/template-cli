3127643ae6d59f2353b55bb98b599dcca4a72475
Rayzhangzhang <1069319374@qq.com>
2023-05-10 13:54:30 +0800

    feat: format

--- src/template/framework/react16/vite/vite.config.js
+++ src/template/framework/react16/vite/vite.config.js
@@ -1,16 +1,14 @@
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
-    plugins: [
-      react(),
-    ],
+    plugins: [react()],
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
     server: {
